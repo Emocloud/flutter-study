4aae800a7037d0096a781aedc6e74174c8fe99ea
Jinho Park <79356772+Emocloud@users.noreply.github.com>
2024-01-28 14:01:13 +0000

    data fetching

--- lib/main.dart
+++ lib/main.dart
@@ -1,30 +1,24 @@
 import 'package:flutter/material.dart';
 import 'package:device_preview/device_preview.dart';
 import 'package:myapp/screens/home_screen.dart';
+import 'package:myapp/services/api_service.dart';
 
-void main() => runApp(
-      DevicePreview(
-        enabled: true,
-        builder: (context) => App(), // Wrap your app
-      ),
-    );
+void main() {
+  ApiService().getTodaysToons();
+  runApp(
+    DevicePreview(
+      enabled: true,
+      builder: (context) => App(), // Wrap your app
+    ),
+  );
+}
 
 class App extends StatelessWidget {
   const App({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSwatch(
-          backgroundColor: const Color(0xFFE7626C),
-        ),
-        textTheme: const TextTheme(
-          displayLarge: TextStyle(
-            color: Color(0xFF232B55),
-          ),
-        ),
-        cardColor: const Color(0xFFF4EDDB),
-      ),
       home: HomeScreen(),
     );
   }
